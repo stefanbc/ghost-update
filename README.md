6e0d8167fd9a7783731a4fff6ed52b1bc606443f
stefanbc <stefcosma@gmail.com>
2016-09-15 23:52:39 +0300

    First actual commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ghost-update
-A small shell script to update your Ghost installation
+# ghoster
+
+A small shell script to update your Ghost installation.
+
+## Before usage
+
+Keep in mind that this script requires cURL to download the latest stable version of Ghost.
+
+Before you start the script make sure you modify the `GHOST_INSTALL_PATH` line with the correct path to your Ghost instance.
+
+After that, this script requires execution rights. You can use this command to enable those `chmod +x ghoster.sh`.
+
+In case your Ghost instance is not running on a Digital Ocean droplet you can comment out line 56 and replace line 62 with the command you use to start your Ghost instance.
+
+## Usage
+
+You can use the `./ghoster help` to read about the available commands.
+
+To actually use the script, you can use the `./ghoster update` command, to update your Ghost instance.
+
+## Notice
+
+Please read the [LICENSE](LICENSE) very carefully as the author of this script should not be held accountable for any damages that this script might cause.
+
---
+++ ghoster.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env fish
+#
+# # Ghoster - Ghost update script
+#
+
+# ## Config
+set -l GHOST_INSTALL_PATH "/Users/stefan/Work/ghost/"
+
+if test (count $argv) -lt 1
+  echo "
+        Ghoster command required. See the following command for help
+            ./ghoster help
+        "
+else
+  switch $argv[1]
+    case "help"
+      echo "
+Commands:
+
+update   # Update Ghost instance
+
+Example of Ghost update comand:
+
+./ghoster update
+"
+      exit 0
+
+    case "update"
+        echo "Step 1: getting the latest Ghost version"
+        and curl -LOk https://ghost.org/zip/ghost-latest.zip
+        and mv ghost-latest.zip /tmp/ghost-latest.zip
+
+        and echo "Step 2: unziping to a temporary location"
+        and unzip /tmp/ghost-latest.zip -d /tmp/ghost-temp
+
+        and echo "Step 3: changing directory into your current ghost install"
+        and cd $GHOST_INSTALL_PATH
+        
+        and echo "Step 4: removing the core directory completely"
+        and rm -rf core
+        
+        and echo "Step 5: changing back to your download of Ghost latest"
+        and cd /tmp/ghost-temp
+        
+        and echo "Step 6: copying the new core directory to your Ghost install"
+        and cp -R core $GHOST_INSTALL_PATH
+        
+        and echo "Step 7: copying the other key files to your Ghost install directory"
+        and cp index.js $GHOST_INSTALL_PATH 
+        and cp *.json $GHOST_INSTALL_PATH
+        
+        and echo "Step 8: changing back to your ghost install directory"
+        and cd $GHOST_INSTALL_PATH
+        
+        and echo "Step 9: updating permissions"
+        and chown -R ghost:ghost *
+        
+        and echo "Step 10: upgrading dependencies"
+        and npm install --production
+        
+        and echo "Step 11: restarting Ghost!"
+        and service restart ghost
+        and rm /tmp/ghost-latest.zip
+        and rm -rf /tmp/ghost-temp
+
+        and echo "Step 12: Ghost update finished!"
+
+        or begin
+            set -l err $status
+            echo "Ghost update encountered an error. Exit status: $err."
+            exit $err
+        end
+  end
+end
